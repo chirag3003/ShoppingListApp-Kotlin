13855c48f3bef5b9b192e7bf799325da7f8c008f
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2024-01-04 05:48:17 +0530

    finished shopping list app

--- app/src/main/java/com/example/shoppinglist/ShoppingList.kt
+++ app/src/main/java/com/example/shoppinglist/ShoppingList.kt
@@ -1,20 +1,136 @@
 package com.example.shoppinglist
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.AbsoluteCutCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+data class ListItem(
+    val name: String,
+    val quantity: Int,
+)
 
 @Composable
 fun ShoppingListApp() {
-    Column(modifier = Modifier.fillMaxSize().padding(10.dp)) {
-        Button(onClick = { /*TODO*/ }) {
-            Text(text = "Add Item")
+    var sItems by remember {
+        mutableStateOf(listOf<ListItem>(ListItem("Hello",4 )))
+    }
+    var showDialog by remember{ mutableStateOf(false) }
+    var itemName by remember{ mutableStateOf("") }
+    var itemQuantity by remember{ mutableStateOf("") }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(10.dp)
+    ) {
+        Button(
+            onClick = { showDialog=true },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(5.dp),
+            shape = RoundedCornerShape(20)
+        ) {
+            Text(text = "Add Item", fontSize = 20.sp)
+        }
+        Spacer(modifier = Modifier.height(5.dp))
+        LazyColumn {
+            items(sItems) {
+                ShoppingListItem(item = it)
+            }
         }
     }
+    if(showDialog){
+        AlertDialog(onDismissRequest = { showDialog = false },
+            confirmButton = {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Button(onClick = {
+                        if (itemName.isNotBlank()) {
+                            val newItem = ListItem(
+                                name = itemName,
+                                quantity = itemQuantity.toInt()
+                            )
+                            sItems = sItems + newItem
+                            showDialog = false
+                            itemName = ""
+                        }
+                    }) {
+                        Text("Add")
+                    }
+                    Button(onClick = { showDialog = false }) {
+                        Text("Cancel")
+                    }
+                }
+
+            },
+            title = { Text("Add Shopping Item") },
+            text = {
+                Column {
+                    OutlinedTextField(
+                        value = itemName,
+                        onValueChange = { itemName = it },
+                        singleLine = true,
+                        placeholder = { Text(text = "Name") },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
+                    )
+
+                    OutlinedTextField(
+                        value = itemQuantity,
+                        onValueChange = { itemQuantity = it },
+                        singleLine = true,
+                        placeholder = { Text(text = "Quantity") },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
+                    )
+                }
+            }
+        )
+    }
+}
+
+@Composable
+fun ShoppingListItem(item: ListItem) {
+    Row(modifier = Modifier
+//        .background(Color.Cyan)
+        .border(2.dp, Color.Cyan, RoundedCornerShape(20))
+        .padding(10.dp)
+    ) {
+        Text(text = item.name, modifier = Modifier.weight(1f))
+        Text(text = item.quantity.toString())
+
+    }
+    Spacer(modifier = Modifier.height(4.dp))
 }
